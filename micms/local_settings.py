2317380131c5c792618bd8fc551bfd1971848505
Debian <fabiiogonzalez8@gmail.com>
2024-01-22 19:17:57 +0000

    repositorio creado

---
+++ micms/local_settings.py
@@ -0,0 +1,28 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "django-insecure-5zzi3ru_@uk8^(f-az@s2vsuhb%d19%m2&#6%c*i1-nebuw59d"
+NEVERCACHE_KEY = "93k_jug(+8&)u6mg(e7%b037*#)q4+p*6u@%))8rgc&hyi7$ff"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+# Allowed development hosts
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1",'*']
